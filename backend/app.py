bf63c4e9a8f10fd28b84a8129a19e36b74148f4c
Himanshu Singh <himanshusingh230103@gmail.com>
2026-01-29 22:06:22 +0530

    env added

--- backend/app.py
+++ backend/app.py
@@ -6,31 +6,22 @@ from routes.project_routes import project_bp
 from routes.freelancer_routes import freelancer_bp
 import os
 
-
 app = Flask(__name__)
 
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
-# ----------------------------
-# CORS FIXED
-# ----------------------------
 CORS(
     app,
-    resources={r"/freelancefusion/*": {"origins": "http://localhost:5173"}},
+    resources={r"/freelancefusion/*": {"origins": FRONTEND_URL}},
     supports_credentials=True,
     allow_headers=["Content-Type", "Authorization"],
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 )
 
-# ----------------------------
-# Register Blueprints
-# ----------------------------
 app.register_blueprint(auth_bp, url_prefix="/freelancefusion")
 app.register_blueprint(user_bp, url_prefix="/freelancefusion")
 app.register_blueprint(project_bp, url_prefix="/freelancefusion")
 app.register_blueprint(freelancer_bp, url_prefix="/freelancefusion")
 
-# ----------------------------
-# Run Server
-# ----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(port=5000)
