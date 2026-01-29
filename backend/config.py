9a84b7ce68bb0aa37e7f81d0446a0973f9821e17
Himanshu Singh <himanshusingh230103@gmail.com>
2026-01-29 22:15:27 +0530

    gunicorn added

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
+
 import os
 
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME", "freelance")
-CLERK_ISSUER = os.getenv("CLERK_ISSUER")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://himanshusingh230103:Himanshu321%40@uberclone.srfhcpy.mongodb.net/?retryWrites=true&w=majority&appName=UberClone")
+DB_NAME = "freelance"
+CLERK_ISSUER = os.getenv("CLERK_ISSUER", "https://organic-swan-82.clerk.accounts.dev")
 JWKS_URL = f"{CLERK_ISSUER}/.well-known/jwks.json"
