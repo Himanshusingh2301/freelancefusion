4c4c636b37071940564e7294ee0a14e152d444e5
Himanshu Singh <himanshusingh230103@gmail.com>
2026-02-16 01:33:42 +0530

    home page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
 import FirstSec from '@/components/FirstSec'
+import FeaturedFreelancer from '@/components/FeaturedFreelancer'
+import ClientFeatures from '@/components/ClientFeatures'
+import AboutSection from '@/components/About'
+import Footer from '@/components/Footer'
+
 const Home = () => {
   return (
     <div>
       <Navbar/>
        <FirstSec/>
+       <FeaturedFreelancer/>
+       <ClientFeatures/>
+       <AboutSection/>
+       <Footer/>
     </div>
   )
 }
